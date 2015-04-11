0e2d569cebcdf01014dcc276204221ce9d0c28ea
Luis Angel Vicente Sanchez <luis.vicente@mindcandy.com>
2015-04-11 14:21:59 +0100

    * minor refactor

--- src/main/scala/com/mindcandy/data/jobs/BaseJob.scala
+++ src/main/scala/com/mindcandy/data/jobs/BaseJob.scala
@@ -5,12 +5,13 @@ import com.typesafe.config.Config
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.DStream
 import org.joda.time.DateTime
-
 import scala.concurrent.duration.FiniteDuration
 
 trait BaseJob {
   def convert: Seq[TypeConverter[_]]
+
   def produce(config: Config, streaming: StreamingContext): DStream[String]
+
   def buckets[A, B](f: A => DateTime, g: A => B, bucket: FiniteDuration)(input: DStream[A]): DStream[(DateTime, B)] =
     input.map { data =>
       val time: DateTime = f(data)
